7c00142c1745412e1e011ee167703d74a68f370e
redamoon <yuuki60824@gmail.com>
2018-11-28 00:41:22 +0900

    [clean] 整理

--- setup-mt7.sh
+++ setup-mt7.sh
@@ -1,11 +1,1 @@
 #!/bin/bash
-
-# 1. Check if .env file exists
-if [ -e .env ]; then
-    source .env
-else
-    echo "Please set up your .env file before starting your enviornment."
-    exit 1
-fi
-
-mkdir -p /var/www/local/hoge/
