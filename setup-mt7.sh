0530b5f17a8c317e059a390f7d5c32605d0105fd
redamoon <yuuki60824@gmail.com>
2018-12-03 23:06:56 +0900

    [modify] 修正

--- setup-mt7.sh
+++ setup-mt7.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Check if .env file exists
+if [ -e .env ]; then
+    source .env
+else
+    echo "Please set up your .env file before starting your enviornment."
+    exit 1
+fi
+
 # Movable Type Setup
